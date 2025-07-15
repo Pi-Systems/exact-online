cfd335e81a90ad593b554111cb378b1a65a4d5fe
Dennis Rombouts <dennis@iboss.nl>
2025-07-15 13:51:59 +0200

    String should still be encapsulated.
    Was not hitting this problem during testing, as all tests I did had all string starting with a letter.
    But if a string starts with a number, it tries to be smart about it, and fails.

--- src/Builder/Edm/UTF8String.php
+++ src/Builder/Edm/UTF8String.php
@@ -3,9 +3,11 @@
 namespace PISystems\ExactOnline\Builder\Edm;
 
 use PISystems\ExactOnline\Model\EdmDataStructure;
+use PISystems\ExactOnline\Model\FilterEncodableDataStructure;
+use PISystems\ExactOnline\Model\TypedValue;
 
 #[\Attribute(flags: \Attribute::TARGET_PROPERTY)]
-class UTF8String extends EdmDataStructure
+class UTF8String extends EdmDataStructure implements FilterEncodableDataStructure
 {
 
     public static function getEdmType(): string
@@ -31,5 +33,11 @@ class UTF8String extends EdmDataStructure
     public function encode(mixed $value): string
     {
         return mb_convert_encoding($value, 'UTF-8', mb_detect_encoding($value));
+    }
+
+
+    public function encodeForFilter(mixed $value): ?TypedValue
+    {
+        return new TypedValue('', $this->encode($value));
     }
 }
